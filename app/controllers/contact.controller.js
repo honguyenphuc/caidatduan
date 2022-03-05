504f8428069f067a3017a835c8e332949f69e289
unknown <honguyenphuc>
2022-03-05 20:11:18 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,20 @@
+exports.create = (req, res) => {
+    res.send({ message: "create handler" });
+exports.findALL = (req, res) => {
+    res.send({ message: "findAll handler" });
+};
+exports.findone = (req, res) => {
+    res.send({ message: "findone handler" });
+};
+exports.update = (req, res) => {
+    res.send({ message: "update handler" });
+};
+exports.delete = (req, res) => {
+    res.send({ message: "delete handler" });
+};
+exports.deleteALL = (req, res) => {
+    res.send({ message: "deleteAll handler" });
+};
+exports.findALLFavorite =
+    res.send({ message: "findAllFavorite handler" });
+}
